fd216ce53cd7669159b41e37d699c3693bb7258d
Sven Wappler <info@wappler.systems>
2022-02-11 15:12:21 +0100

    first commit

---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_bootstraptable/Configuration/TsConfig/rte.tsconfig">');
